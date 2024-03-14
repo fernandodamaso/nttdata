88ebb359f687f1061bc9646b8ffdcafcdeabc27a
Fernando Dâmaso <fernando.yarrum@gmail.com>
2024-03-14 08:34:54 -0300

    Refactor search functionality and add movie list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,18 +4,42 @@ import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 import { MovieComponent } from "../movie/movie.component";
 import { MoviesService } from "../_services/movies.service";
+import { FormsModule } from "@angular/forms";
+import { MovieModel } from "../_models/movie.model";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent, MovieComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent, MovieComponent, FormsModule],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class HomeComponent implements OnInit {
+  movieSearch = "";
+  movieList: MovieModel[] = [];
+  selectedMovie: MovieModel | undefined;
   constructor(private MoviesService: MoviesService) {}
-  ngOnInit(): void {
-    this.MoviesService.searchMovie("batman").then((res) => console.log(res));
+
+  ngOnInit(): void {}
+
+  searchMovie(search: string) {
+    console.log(search);
+    this.MoviesService.searchMovie(search).then((data) => {
+      console.log(data);
+      this.movieList = data.Search;
+    });
+  }
+
+  getMovie(movie: MovieModel) {
+    console.log(movie);
+    this.selectedMovie = movie;
+    this.movieList = [];
+  }
+
+  reset() {
+    this.movieSearch = "";
+    this.movieList = [];
+    this.selectedMovie = undefined;
   }
 }
